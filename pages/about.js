bbe9b06dce441d59f891fbab4b08c4fe4289a491
mwalcher3 <melisande.walcher@gmail.com>
2022-01-03 09:25:45 +0100

    irgentwas

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+           <Head>
+               <title>about</title>
+            </Head> 
+        </div>
+    )
+}
+
+export default about
