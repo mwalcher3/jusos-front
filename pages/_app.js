2d6306b271733142010e5e551b8bfd3df42d4479
mwalcher3 <melisande.walcher@gmail.com>
2022-01-03 09:23:46 +0100

    '

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+    <>
+    <h1> Jusos Heidelberg</h1>
+  <Component {...pageProps} />
+  </>
+  
+  )
 }
 
 export default MyApp
