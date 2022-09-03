2a4522c18ff7157462bf6877698556114be69502
mwalcher3 <melisande.walcher@protonmail.com>
2022-09-03 18:14:36 +0200

    team page update

--- components/SprecherKreis.js
+++ components/SprecherKreis.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import Image from "next/image"
+import {global} from '../pages/_app'
 import skcss from "../styles/component-modules/sprecherkreis.module.scss"
 
-const SprecherKreis = ({data}) => {
-    console.log(data);
+const SprecherKreis = ({data, alternativeImage}) => {
   return (
-    <div className={skcss.circlecontainer}>
-        asfh
-    </div>
+    <ul className={skcss.circlecontainer}>
+        {data.map((item, index)=>{
+            const previewImage= item.attributes.previewImage.data;
+            return(
+                <>
+                <li className={skcss.memberboxes} key={index}>
+
+                    <div className={skcss.roundimages}>
+                    <Image
+                        src={`${global.host}${previewImage!=null? previewImage.attributes.url: alternativeImage.url}`}
+                        alt={`${global.host}${previewImage!=null? previewImage.attributes.alternativeText: alternativeImage.alternativeText}`}
+                        layout="fill"
+                        objectFit="cover"
+                        priority
+                    />
+                    </div>
+
+                    <div className={skcss.name}>
+                    {item.attributes.Name}
+                    </div>
+                </li>
+               </>
+            )
+        })}
+    </ul>
   )
 }
 
--- components/Team/index.js
+++ components/Team/index.js
@@ -5,15 +5,14 @@ import Image from 'next/image'
 import Sprecherkreis from '../SprecherKreis'
 
 const Team = ({data}) => {
-  console.log(data);
     const dataAttributes= data.data.attributes
+    const alternativeImage= dataAttributes.alternativeImageMembers.data.attributes
 
-
-    const sprecher= ["hello"]
-    const otherMembers= ["hello"]
+    const sprecher= []
+    const otherMembers= []
 
     data.memData.forEach((item)=>{
-        if (item.attributes.Rolle=="Sprecher"){
+        if (item.attributes.role=="Sprecher"){
             sprecher.push(item)
         }
         else{
@@ -22,12 +21,11 @@ const Team = ({data}) => {
     })
 
 
-
   return (
     <div className="container">
         <div className="mainimage">
         <Image
-          src={`${global.host}${dataAttributes.image.data.attributes.url}`}
+          src={`${global.host}${dataAttributes.image.data.attributes.formats.large.url}`}
           alt={`${global.host}${dataAttributes.image.data.attributes.alternativeText}`}
           layout="fill"
           objectFit="cover"
@@ -35,8 +33,33 @@ const Team = ({data}) => {
           priority
       />
         </div>
+        <h1>{dataAttributes.title}</h1>
+        <p className={teamcss.description}>{dataAttributes.description}</p>
 
-        <Sprecherkreis data={sprecher}/>
+        <Sprecherkreis data={sprecher} alternativeImage={alternativeImage}/>
+
+        <section className={teamcss.othermembers}>
+          {otherMembers.map((item, index)=>{
+            console.log(item);
+            const previewImage= item.attributes.previewImage.data;
+
+             return(
+              <div key={index}>
+            <div className={teamcss.squareimages}>
+                {<Image
+                src={`${global.host}${previewImage!=null?previewImage.attributes.url : alternativeImage.url}`}
+                alt={`image of a person`}
+                layout="fill"
+                objectFit="cover"
+                priority
+              />}
+         </div>
+         <h4>{item.attributes.Name}</h4>
+         </div>
+          )
+          })}
+          </section>
+      
     </div>
   )
 }
