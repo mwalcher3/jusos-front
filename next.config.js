bc5569f7b54a880b1ba8c950ed2d92e5241cf3b2
mwalcher3 <melisande.walcher@protonmail.com>
2022-10-15 13:15:17 +0200

    💥  ⬆️ ✨ pwa added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+
+module.exports = withPWA({
+  pwa:{
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['content.jusoshd.uber.space', 'scontent-muc2-1.cdninstagram.com'],
   },
-}
+})
 
 
 
