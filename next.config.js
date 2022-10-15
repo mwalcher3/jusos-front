6c86f950f4fa46e83f8bd2ae13cbecaa34982ced
mwalcher3 <melisande.walcher@gmail.com>
2022-10-15 20:04:22 +0200

    hello

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['content.jusoshd.uber.space', 'scontent-muc2-1.cdninstagram.com'],
+    domains: ['content.jusoshd.uber.space',"scontent-frx5-1.cdninstagram.com", "scontent-frt3-1.cdninstagram.com",'scontent-frt3-2.cdninstagram.com','scontent-frx5-2.cdninstagram.com', 'scontent-muc2-1.cdninstagram.com'],
   },
 }
 
