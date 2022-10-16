e6852411344dbde1873de95286fbb8eaab6bdc5a
jwalcher <walcher@uni-heidelberg.de>
2022-10-16 16:46:50 +0200

    🐛 inc

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
-      <link rel="manifest" href='./manifest/manifest.json'/>
+      <link rel="manifest" href="./public/manifest/manifest.json"/>
       <link rel="icon" type="image/png" href="./public/Jusos_Heidelberg_Logo.png"/>
        </Head>
       <body id="body">
