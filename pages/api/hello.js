bf1460fefcc7f42de69342d76980e1f02fd3efca
jwalcher <walcher@uni-heidelberg.de>
2023-07-08 18:22:13 +0200

    save

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import PDFParser from "pdf2json";
+const fs = require("fs");
 
-const fs = require('fs'),
-PDFParser = require("pdf2json");
+// PDFParser = require("pdf2json");
 
 const pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", errData => console.error("error parsing the file: ",errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-
-});
+pdfParser.on("pdfParser_dataError", (errData) =>
+  console.error("error parsing the file: ", errData.parserError)
+);
+pdfParser.on("pdfParser_dataReady", (pdfData) => {});
 
 pdfParser.loadPDF("./public/Satzung.pdf");
 
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-
-
+  res.status(200).json({ name: "John Doe" });
 }
--- pages/api/pdfparser.js
+++ pages/api/pdfparser.js
@@ -1,33 +1,25 @@
+import PDFParser from "pdf2json";
+// const {kColors, kFontFaces, kFontStyles} = require("pdf2json/lib/pdfconst");
 
+// import { kColors, kFontFaces, kFontStyles } from "pdf2json";
 
+const fs = require("fs");
 
 export default function handler(req, res) {
-
-  const fs = require('fs'),
-  PDFParser = require("pdf2json");
-  const {kColors, kFontFaces, kFontStyles} = require("pdf2json/lib/pdfconst");
   const { method } = req;
 
   const pdfParser = new PDFParser();
-  
 
-  pdfParser.on("pdfParser_dataError", errData => console.error("error parsing the file: ",errData.parserError) );
-  pdfParser.on("pdfParser_dataReady", pdfData => {
-  
-  
+  pdfParser.on("pdfParser_dataError", (errData) =>
+    console.error("error parsing the file: ", errData.parserError)
+  );
+  pdfParser.on("pdfParser_dataReady", (pdfData) => {
+    const data = pdfData;
 
-  const data= pdfData
+    if (method === "GET") {
+      return res.status(200).json({ name: data });
+    }
+  });
 
-
-  if (method === "GET") {
-    return res.status(200).json({name: data });
-  }
-
-
-
-});
-
-pdfParser.loadPDF("./public/SebastianOBWahl.pdf");
-
-
+  pdfParser.loadPDF("./public/SebastianOBWahl.pdf");
 }
