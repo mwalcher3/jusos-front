0b4646f980c020444b5bebbfe3ee236f771abf9f
jwalcher <walcher@uni-heidelberg.de>
2023-07-31 18:32:03 +0200

    v13.2.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  experimental: {
+    appDir: true,
+    isrMemoryCacheSize: 50,
+  },
   reactStrictMode: true,
   images: {
     unoptimized: true,
